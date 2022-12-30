cd8d91f48df39a166dde00b0c833ba355028ec5b
Olga Ukropova <olgaukropova@yandex.ru>
2022-12-31 02:20:40 +0300

    иттерация 2

--- script/index.js
+++ script/index.js
@@ -2,29 +2,28 @@ const popupElement = document.querySelector('.popup');
 const openPopupElement = document.querySelector('.profile__button-pen');
 const closePopupElement = document.querySelector('.popup__button-close');
 let form = document.querySelector('.popup__form');
-const h = document.querySelector('.profile__title');
-const p = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const nameInput = form.querySelector('[name="name"]');
+const jobInput = form.querySelector('[name="job"]');
+const closePopupElementSubmit = document.querySelector('.popup__button-submit');
+const dataName = document.querySelector('.popup__field_data_name');
+const dataJob = document.querySelector('.popup__field_data_job');
 const openPopup = function () {
+  dataName.value = profileTitle.textContent;
+  dataJob.value = profileSubtitle.textContent;
+
   popupElement.classList.add('popup_opened');
-  console.log('open popup clicked');
 }
 const closePopup = function () {
   popupElement.classList.remove('popup_opened');
 }
+function getFormValue(event) {
+  event.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+}
 openPopupElement.addEventListener('click', openPopup);
 closePopupElement.addEventListener('click', closePopup);
 form.addEventListener('submit', getFormValue);
-function getFormValue(event) {
-  event.preventDefault();
-  const name = form.querySelector('[name="name"]')
-  job = form.querySelector('[name="job"]')
-  const data = {
-    name: name.value,
-    job: job.value
-  };
-  /*h.innerHTML = data.name;
-  p.innerHTML = data.job*/
-  h.textContent = data.name;
-  p.textContent = data.job;
-}
-
+closePopupElementSubmit.addEventListener('click', closePopup);
