21b0e506b65e1d42e44f4c25dac02103533768a6
Olga Ukropova <olgaukropova@yandex.ru>
2023-01-03 17:13:09 +0300

    иттерация3

--- script/index.js
+++ script/index.js
@@ -4,15 +4,12 @@ const closePopupElement = document.querySelector('.popup__button-close');
 let form = document.querySelector('.popup__form');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const nameInput = form.querySelector('[name="name"]');
-const jobInput = form.querySelector('[name="job"]');
 const closePopupElementSubmit = document.querySelector('.popup__button-submit');
 const dataName = document.querySelector('.popup__field_data_name');
 const dataJob = document.querySelector('.popup__field_data_job');
 const openPopup = function () {
   dataName.value = profileTitle.textContent;
   dataJob.value = profileSubtitle.textContent;
-
   popupElement.classList.add('popup_opened');
 }
 const closePopup = function () {
@@ -20,10 +17,11 @@ const closePopup = function () {
 }
 function getFormValue(event) {
   event.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
+  profileTitle.textContent = dataName.value;
+  profileSubtitle.textContent = dataJob.value;
+  closePopup();
 }
 openPopupElement.addEventListener('click', openPopup);
 closePopupElement.addEventListener('click', closePopup);
 form.addEventListener('submit', getFormValue);
-closePopupElementSubmit.addEventListener('click', closePopup);
+
